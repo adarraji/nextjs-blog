1d2218a35b1c840928692e6d0e0b7faeda6de012
adarraji <a.darraji@outlook.com>
2023-07-01 14:26:13 -0400

    added pages links

--- src/components/navbar/Navbar.jsx
+++ src/components/navbar/Navbar.jsx
@@ -1,9 +1,52 @@
+import Link from 'next/link'
 import React from 'react'
 
+const links = [
+    {
+        id: 1,
+        title: "Home",
+        url: "/",
+    },
+    {
+        id: 2,
+        title: "Portfolio",
+        url: "/portfolio",
+    },
+    {
+        id: 3,
+        title: "Blog",
+        url: "/blog",
+    },
+    {
+        id: 4,
+        title: "About",
+        url: "/about",
+    },
+    {
+        id: 5,
+        title: "Contact",
+        url: "/contact",
+    },
+    {
+        id: 6,
+        title: "Dashboard",
+        url: "/dashboard",
+    },
+];
+
 const Navbar = () => {
-  return (
-    <div>Navbar</div>
-  )
+    return (
+        <div>
+            <Link href="/">AD DEV</Link>
+            <div>
+                {
+                    links.map(link => (
+                        <Link key={link.id} href={link.url}>{link.title}</Link>
+                    ))
+                }
+            </div>
+        </div>
+    )
 }
 
 export default Navbar
