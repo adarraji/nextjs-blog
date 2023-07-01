e73ee1eeab09bcde3f4918b291ccacfe297c31c4
adarraji <a.darraji@outlook.com>
2023-07-01 14:10:47 -0400

    added layout

---
+++ src/app/blog/layout.jsx
@@ -0,0 +1,12 @@
+import React from 'react'
+
+const BlogLayout = ({ children }) => {
+    return (
+        <div>
+            <h2>Hi this is blog layout</h2>
+            {children}
+        </div>
+    )
+}
+
+export default BlogLayout
