2237c4b1f22b5cb74403d90612a9cd83693badd7
adarraji <a.darraji@outlook.com>
2023-07-01 14:29:23 -0400

    added footer text

--- src/components/footer/Footer.jsx
+++ src/components/footer/Footer.jsx
@@ -2,7 +2,10 @@ import React from 'react'
 
 const Footer = () => {
   return (
-    <div>Footer</div>
+    <div>
+      <div>©2023 AD DEV. All rights reserved.</div>
+      <div></div>
+    </div>
   )
 }
 
