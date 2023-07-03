e29756f31a43df6d51925181a677563344b405b5
adarraji <a.darraji@outlook.com>
2023-07-03 16:37:45 -0400

    added styles

--- src/components/footer/Footer.jsx
+++ src/components/footer/Footer.jsx
@@ -1,11 +1,17 @@
 import React from 'react'
 import styles from "./page.module.css"
+import Image from 'next/image'
 
 const Footer = () => {
   return (
-    <div>
+    <div className={styles.container}>
       <div>©2023 AD DEV. All rights reserved.</div>
-      <div className={styles.container}></div>
+      <div className={styles.social}>
+        <Image src="/1.png" width={15} height={15} className={styles.icon} alt="AD DEV Facebook Account" />
+        <Image src="/2.png" width={15} height={15} className={styles.icon} alt="AD DEV" />
+        <Image src="/3.png" width={15} height={15} className={styles.icon} alt="AD DEV" />
+        <Image src="/4.png" width={15} height={15} className={styles.icon} alt="AD DEV" />
+      </div>
     </div>
   )
 }
