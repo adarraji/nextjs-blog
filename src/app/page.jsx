313eb129b2454ae55431d2ea59b4a84e70141004
adarraji <a.darraji@outlook.com>
2023-07-08 16:10:35 -0400

    added img to home page

--- src/app/page.jsx
+++ src/app/page.jsx
@@ -1,10 +1,11 @@
 import Image from 'next/image'
 import styles from './page.module.css'
+import Hero from "public/hero.png"
 
 export default function Home() {
   return (
     <div className={styles.container}>
-      Home
+      <Image src={Hero} alt="" className={styles.img} />
     </div>
   )
 }
