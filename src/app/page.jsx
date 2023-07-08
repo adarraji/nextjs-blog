c174bc36cd7b43ff1c86bc5553e3ea825278243f
adarraji <a.darraji@outlook.com>
2023-07-08 16:22:10 -0400

    added title, decr, button, styles

--- src/app/page.jsx
+++ src/app/page.jsx
@@ -5,7 +5,17 @@ import Hero from "public/hero.png"
 export default function Home() {
   return (
     <div className={styles.container}>
-      <Image src={Hero} alt="" className={styles.img} />
+      <div className={styles.item}>
+        <h1 className={styles.title}>Better design for your digital products.</h1>
+        <p className={styles.desc}>
+          Turning your Idea into Reality. We bring together the teams from the
+          global tech industry.
+        </p>
+        <button className={styles.button}>See Our Works</button>
+      </div>
+      <div className={styles.item}>
+        <Image src={Hero} alt="" className={styles.img} />
+      </div>
     </div>
   )
 }
