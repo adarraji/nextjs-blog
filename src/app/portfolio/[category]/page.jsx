a70268f707f2e2a0823433844d24b2f3743f9880
adarraji <a.darraji@outlook.com>
2023-07-11 14:14:17 -0400

    added portfolio cat elements and styles

--- src/app/portfolio/[category]/page.jsx
+++ src/app/portfolio/[category]/page.jsx
@@ -1,8 +1,43 @@
 import React from 'react'
+import styles from "./page.module.css"
+import Button from '@/components/button/Button'
+import Image from 'next/image'
+import { items } from './data'
+import { notFound } from 'next/navigation'
 
-const Category = () => {
+const getData = (cat) => {
+    const data = items[cat]
+
+    if (data) {
+        return data
+    }
+
+    return notFound()
+
+}
+
+const Category = ({ params }) => {
+    const data = getData(params.category)
+
+    console.log(params)
     return (
-        <div>Category</div>
+        <div className={styles.container}>
+            <h1 className={styles.catTitle}>{params.category}</h1>
+            {
+                data.map(item => (
+                    <div className={styles.item} key={item.id}>
+                        <div className={styles.content}>
+                            <h1 className={styles.title} >{item.title}</h1>
+                            <p className={styles.desc} >{item.desc}</p>
+                            <Button url="#" text="See More" />
+                        </div>
+                        <div className={styles.imgContainer}>
+                            <Image className={styles.img} fill={true} src={item.image} alt="" />
+                        </div>
+                    </div>
+                ))
+            }
+        </div>
     )
 }
 
