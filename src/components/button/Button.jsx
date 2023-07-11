5eb3df3c476a37e012fee818bc9b46fb75119299
adarraji <a.darraji@outlook.com>
2023-07-10 21:58:37 -0400

    adding button styles

--- src/components/button/Button.jsx
+++ src/components/button/Button.jsx
@@ -5,7 +5,7 @@ import Link from 'next/link'
 const Button = ({ text, url }) => {
     return (
         <Link href={url}>
-            <button className={styles.container}>{text}</button>
+            <button>{text}</button>
         </Link>
     )
 }
