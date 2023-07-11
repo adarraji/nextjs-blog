f265cc3e7bb8b83dd6cf906de2f62280c6b04334
adarraji <a.darraji@outlook.com>
2023-07-10 21:32:21 -0400

    added about page comp.

--- src/app/about/page.jsx
+++ src/app/about/page.jsx
@@ -1,9 +1,15 @@
 import React from 'react'
 import styles from "./page.module.css"
+import Image from 'next/image'
 
 const About = () => {
   return (
-    <div className={styles.container}>About</div>
+    <div className={styles.container}>
+      <div className={styles.imgContainer}>
+        <Image className={styles.img} src="https://images.pexels.com/photos/3194521/pexels-photo-3194521.jpeg?auto=compress&cs=tinysrgb&w=1260&h=750&dpr=2" fill={true} alt="" />
+      </div>
+      <div className={styles.textContainer}></div>
+    </div>
   )
 }
 
