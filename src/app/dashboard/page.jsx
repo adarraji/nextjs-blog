8b4ebf5d47ec14e4f2dc52ce38157ee26ca8180e
adarraji <a.darraji@outlook.com>
2023-07-12 19:23:10 -0400

    fetching data client side

--- src/app/dashboard/page.jsx
+++ src/app/dashboard/page.jsx
@@ -1,7 +1,38 @@
-import React from 'react'
+"use client"
+import React, { useEffect, useState } from 'react'
 import styles from "./page.module.css"
+import useSWR from 'swr'
 
 const Dashboard = () => {
+    // const [data, setData] = useState([])
+    // const [error, setError] = useState(false)
+    // const [isLoading, setIsLoading] = useState(false)
+
+    // useEffect(() => {
+    //     const getData = async () => {
+    //         setIsLoading(true)
+    //         const res = await fetch("https://jsonplaceholder.typicode.com/posts", { cache: 'no-store' })
+
+    //         if (!res.ok) {
+    //             setError(true)
+    //         }
+
+    //         const data = await res.json()
+    //         setData(data)
+    //         setIsLoading(false)
+    //     }
+
+    //     getData()
+    // }, [])
+
+
+    const fetcher = (...args) => fetch(...args).then(res => res.json())
+
+    const { data, error, isLoading } = useSWR("https://jsonplaceholder.typicode.com/posts", fetcher)
+
+    console.log(data)
+
+
     return (
         <div className={styles.container}>Dashboard</div>
     )
