d58370a2c9d372cb4d359b651c9d1fd675c96571
adarraji <a.darraji@outlook.com>
2023-07-12 16:05:29 -0400

    fethcing data into blog post page

--- src/app/blog/[id]/page.jsx
+++ src/app/blog/[id]/page.jsx
@@ -1,13 +1,26 @@
 import React from 'react'
 import styles from "./page.module.css"
 import Image from 'next/image'
+import { notFound } from 'next/navigation'
 
-const BlogPost = () => {
+
+async function getData(id) {
+    const res = await fetch(`https://jsonplaceholder.typicode.com/posts/${id}`, { cache: 'no-store' })
+
+    if (!res.ok) {
+        return notFound()
+    }
+
+    return res.json()
+}
+
+const BlogPost = async ({ params }) => {
+    const data = await getData(params.id)
     return (
         <div className={styles.container}>
             <div className={styles.top}>
                 <div className={styles.info}>
-                    <h1 className={styles.title}>Lorem ipsum dolor sit amet consectetur adipisicing elit.</h1>
+                    <h1 className={styles.title}>{data.title}</h1>
                     <p className={styles.desc}>Lorem ipsum dolor sit amet consectetur adipisicing elit. Dignissimos delectus ipsam quam nisi, dolorum velit quod tempore debitis fugit obcaecati ratione expedita error optio ut magnam odit nam sapiente impedit.</p>
                     <div className={styles.author}>
                         <Image className={styles.avatar} height={40} width={40} src="https://images.pexels.com/photos/16770473/pexels-photo-16770473/free-photo-of-hand-holding-clove.jpeg?auto=compress&cs=tinysrgb&w=400&lazy=loads" alt="" />
