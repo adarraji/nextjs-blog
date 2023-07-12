268e8e700151530df213bc50f32c52a0dc8e8bc3
adarraji <a.darraji@outlook.com>
2023-07-11 20:48:59 -0400

    adding blog page elements

--- src/app/blog/page.jsx
+++ src/app/blog/page.jsx
@@ -1,10 +1,40 @@
 import React from 'react'
 import styles from "./page.module.css"
+import Link from 'next/link'
+import Image from 'next/image'
 
-const Blog = () => {
+const page = () => {
     return (
-        <div className={styles.container}>Blog</div>
+        <div className={styles.mainContainer}>
+            <Link href="/blog/testId" className={styles.container}>
+                <div className={styles.imageContainer}>
+                    <Image src="https://images.pexels.com/photos/2103127/pexels-photo-2103127.jpeg" alt="" width={400} height={250} className={styles.image} />
+                </div>
+                <div className={styles.content}>
+                    <h1 className={styles.title}></h1>
+                    <p className={styles.desc}></p>
+                </div>
+            </Link>
+            <Link href="/blog/testId" className={styles.container}>
+                <div className={styles.imageContainer}>
+                    <Image src="https://images.pexels.com/photos/2103127/pexels-photo-2103127.jpeg" alt="" width={400} height={250} className={styles.image} />
+                </div>
+                <div className={styles.content}>
+                    <h1 className={styles.title}></h1>
+                    <p className={styles.desc}></p>
+                </div>
+            </Link>
+            <Link href="/blog/testId" className={styles.container}>
+                <div className={styles.imageContainer}>
+                    <Image src="https://images.pexels.com/photos/2103127/pexels-photo-2103127.jpeg" alt="" width={400} height={250} className={styles.image} />
+                </div>
+                <div className={styles.content}>
+                    <h1 className={styles.title}></h1>
+                    <p className={styles.desc}></p>
+                </div>
+            </Link>
+        </div>
     )
 }
 
-export default Blog
+export default page
