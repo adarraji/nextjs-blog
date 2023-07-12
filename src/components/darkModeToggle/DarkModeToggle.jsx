28c26837821fe0837750f7373f9e29597c33452e
adarraji <a.darraji@outlook.com>
2023-07-12 15:28:36 -0400

    added toggle styles

--- src/components/darkModeToggle/DarkModeToggle.jsx
+++ src/components/darkModeToggle/DarkModeToggle.jsx
@@ -2,11 +2,12 @@ import React from 'react'
 import styles from "./darkModeToggle.module.css"
 
 export const DarkModeToggle = () => {
+    const mode = "light"
     return (
         <div className={styles.container}>
             <div className={styles.icon}>🌙</div>
             <div className={styles.icon}>🔆</div>
-            <div className={styles.ball}></div>
+            <div className={styles.ball} style={mode === "light" ? { left: "2px" } : { right: "2px" }}></div>
         </div>
     )
 }
