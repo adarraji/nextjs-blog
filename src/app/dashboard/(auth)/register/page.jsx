bb77062254faf71c0a817c53b9cd5f1082282f96
adarraji <a.darraji@outlook.com>
2023-07-13 18:38:42 -0400

    updating  register page

--- src/app/dashboard/(auth)/register/page.jsx
+++ src/app/dashboard/(auth)/register/page.jsx
@@ -1,17 +1,36 @@
 "use client"
-import React from 'react'
+import React, { useState } from 'react'
 import styles from "./page.module.css"
 import Link from 'next/link'
 
 const Register = () => {
+
+    const [error, setError] = useState(false)
+
+    const handleSubmit = async (e) => {
+        e.preventDefault()
+        const name = e.target[0].value
+        const email = e.target[1].value
+        const password = e.target[2].value
+
+        const res = await fetch("/api/auth/resgiter")
+
+        try {
+
+        } catch (err) {
+            setError(true)
+        }
+    }
+
     return (
         <div className={styles.container}>
-            <form className={styles.form}>
+            <form className={styles.form} onSubmit={handleSubmit}>
                 <input type="text" placeholder="username" className={styles.input} required />
                 <input type="email" placeholder="email" className={styles.input} required />
                 <input type="password" placeholder="password" className={styles.input} required />
                 <button className={styles.button}>Register</button>
             </form>
+            {error && "Something went wrong!"}
             <Link href="/dashboard/login">Login with an existing account</Link>
         </div>
     )
