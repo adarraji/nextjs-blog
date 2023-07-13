8de9f1a2d5ac5e63d81aa3abf8a3269af565ad55
adarraji <a.darraji@outlook.com>
2023-07-13 16:54:45 -0400

    added metadata for blog post

--- src/app/blog/[id]/page.jsx
+++ src/app/blog/[id]/page.jsx
@@ -14,6 +14,15 @@ async function getData(id) {
     return res.json()
 }
 
+
+export async function generateMetadata({ params }) {
+    const post = await getData(params.id)
+    return {
+        title: post.title,
+        description: post.desc,
+    }
+}
+
 const BlogPost = async ({ params }) => {
     const data = await getData(params.id)
     return (
