057b72d0a11d8381d4b368736b481b2f5caa0fd6
adarraji <a.darraji@outlook.com>
2023-07-13 16:50:55 -0400

    added metadata, page title and desc for contact page

--- src/app/contact/page.jsx
+++ src/app/contact/page.jsx
@@ -3,6 +3,11 @@ import styles from "./page.module.css"
 import Image from 'next/image'
 import Button from '@/components/button/Button'
 
+export const metadata = {
+    title: 'AD DEV Contact Page',
+    description: 'This is Contact page',
+}
+
 const Contact = () => {
     return (
         <div className={styles.container}>
