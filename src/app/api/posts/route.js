33cb59fcb9295446edb1ce7bd6656e1e2fde33a3
adarraji <a.darraji@outlook.com>
2023-07-12 20:30:14 -0400

    updated posts model and api route

--- src/app/api/posts/route.js
+++ src/app/api/posts/route.js
@@ -1,8 +1,18 @@
 import { NextResponse } from "next/server"
+import connect from "@/utils/db"
+import Post from "@/models/Post";
 
 export const GET = async (request) => {
-    //fetch
+    try {
+        await connect();
 
-    return new NextResponse("It works!", { status: 200 })
+        const posts = await Post.find()
+        return new NextResponse(posts, { status: 200 })
+
+    } catch (err) {
+        return new NextResponse("Database Error", { status: 500 })
+    }
+
+
 
 }
