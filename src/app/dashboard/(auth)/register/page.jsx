e75d47d8b69e1bac183f3fffde5e3ae0a76865de
adarraji <a.darraji@outlook.com>
2023-07-13 18:27:23 -0400

    creating register page

--- src/app/dashboard/(auth)/register/page.jsx
+++ src/app/dashboard/(auth)/register/page.jsx
@@ -1,8 +1,19 @@
+"use client"
 import React from 'react'
+import styles from "./page.module.css"
+import Link from 'next/link'
 
 const Register = () => {
     return (
-        <div>Register</div>
+        <div className={styles.container}>
+            <form className={styles.form}>
+                <input type="text" placeholder="username" className={styles.input} required />
+                <input type="email" placeholder="email" className={styles.input} required />
+                <input type="password" placeholder="password" className={styles.input} required />
+                <button className={styles.button}>Register</button>
+            </form>
+            <Link href="/dashboard/login">Login with an existing account</Link>
+        </div>
     )
 }
 
