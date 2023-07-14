8b2688d575712417cc4dc936a88a6154aa1410c2
adarraji <a.darraji@outlook.com>
2023-07-14 13:02:30 -0400

    added login using credential

--- src/app/api/auth/[...nextauth]/route.js
+++ src/app/api/auth/[...nextauth]/route.js
@@ -1,5 +1,8 @@
+import User from "@/models/User"
+import connect from "@/utils/db"
 import NextAuth from "next-auth"
 import GoogleProvider from "next-auth/providers/google"
+import bcrypt from "bcryptjs";
 
 const handler = NextAuth({
     providers: [
@@ -7,6 +10,33 @@ const handler = NextAuth({
             clientId: process.env.GOOGLE_CLIENT_ID,
             clientSecret: process.env.GOOGLE_CLIENT_SECRET,
         }),
+        CredentialsProvider({
+            id: "credentials",
+            name: "credentials",
+            async authorize(credentials) {
+
+                await connect()
+
+                try {
+                    const user = User.findOne({ email: credentials.emaail })
+                    if (user) {
+                        //check password
+                        const isPasswordCorrect = bcrypt.compare(credentials.password, user.password)
+                        if (isPasswordCorrect) {
+                            return user
+                        } else {
+                            throw new Error("Wrong Credentials")
+                        }
+                    } else {
+                        throw new Error("User not found")
+                    }
+
+                } catch (err) {
+                    throw new Error(err)
+                }
+
+            }
+        })
     ],
 })
 
