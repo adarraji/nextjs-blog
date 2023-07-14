203c9ed80f77ab65ac22f571dbfa1818bd3ba377
adarraji <a.darraji@outlook.com>
2023-07-14 09:18:45 -0400

    updaing register route

--- src/app/dashboard/(auth)/register/page.jsx
+++ src/app/dashboard/(auth)/register/page.jsx
@@ -1,51 +1,71 @@
-"use client"
-import React, { useState } from 'react'
-import styles from "./page.module.css"
-import Link from 'next/link'
-import { useRouter } from 'next/navigation'
-
+"use client";
+import React, { useState } from "react";
+import styles from "./page.module.css";
+import Link from "next/link";
+import { useRouter } from "next/navigation";
 
 const Register = () => {
+    const [error, setError] = useState(null);
 
-    const [error, setError] = useState(false)
-
-    const router = useRouter()
+    const router = useRouter();
 
     const handleSubmit = async (e) => {
-        e.preventDefault()
-        const name = e.target[0].value
-        const email = e.target[1].value
-        const password = e.target[2].value
-
-        const res = await fetch("/api/auth/resgiter", {
-            method: "POST",
-            headers: {
-                "Content-Type": "application/json"
-            },
-            body: JSON.stringify({ name, email, password })
-        })
-
-        res.status === 201 && router.push("/dashboard/login?success=Account has been created")
+        e.preventDefault();
+        const name = e.target[0].value;
+        const email = e.target[1].value;
+        const password = e.target[2].value;
 
         try {
-
+            const res = await fetch("/api/auth/register", {
+                method: "POST",
+                headers: {
+                    "Content-Type": "application/json",
+                },
+                body: JSON.stringify({
+                    name,
+                    email,
+                    password,
+                }),
+            });
+            res.status === 201 && router.push("/dashboard/login?success=Account has been created");
         } catch (err) {
-            setError(true)
+            setError(err);
+            console.log(err);
         }
-    }
+    };
 
     return (
         <div className={styles.container}>
-            <form className={styles.form} onSubmit={handleSubmit}>
-                <input type="text" placeholder="username" className={styles.input} required />
-                <input type="email" placeholder="email" className={styles.input} required />
-                <input type="password" placeholder="password" className={styles.input} required />
+            <h1 className={styles.title}>Create an Account</h1>
+            <h2 className={styles.subtitle}>Please sign up to see the dashboard.</h2>
+            <form onSubmit={handleSubmit} className={styles.form}>
+                <input
+                    type="text"
+                    placeholder="Username"
+                    required
+                    className={styles.input}
+                />
+                <input
+                    type="text"
+                    placeholder="Email"
+                    required
+                    className={styles.input}
+                />
+                <input
+                    type="password"
+                    placeholder="Password"
+                    required
+                    className={styles.input}
+                />
                 <button className={styles.button}>Register</button>
+                {error && "Something went wrong!"}
             </form>
-            {error && "Something went wrong!"}
-            <Link href="/dashboard/login">Login with an existing account</Link>
+            <span className={styles.or}>- OR -</span>
+            <Link className={styles.link} href="/dashboard/login">
+                Login with an existing account
+            </Link>
         </div>
-    )
-}
+    );
+};
 
-export default Register
+export default Register;
