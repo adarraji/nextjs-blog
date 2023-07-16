2ba15a652c4d6cd5444bdb4c63926d628bd972f8
adarraji <a.darraji@outlook.com>
2023-07-16 14:44:42 -0400

    added delete route

--- src/app/api/posts/[id]/route.js
+++ src/app/api/posts/[id]/route.js
@@ -15,3 +15,17 @@ export const GET = async (request, { params }) => {
         return new NextResponse("Database Error", { status: 500 })
     }
 }
+
+export const DELETE = async (request, { params }) => {
+    const { id } = params;
+
+    try {
+        await connect();
+
+        await Post.findByIdAndDelete(id);
+
+        return new NextResponse("Post has been deleted", { status: 200 });
+    } catch (err) {
+        return new NextResponse("Database Error", { status: 500 });
+    }
+};
