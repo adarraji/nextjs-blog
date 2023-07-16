ed471bd8a96474fada5bf6467ffacdb541a8cb26
adarraji <a.darraji@outlook.com>
2023-07-16 14:43:53 -0400

    admin dashboard

--- src/app/api/posts/route.js
+++ src/app/api/posts/route.js
@@ -18,3 +18,20 @@ export const GET = async (request) => {
         return new NextResponse("Database Error", { status: 500 })
     }
 }
+
+
+export const POST = async (request) => {
+    const body = await request.json();
+
+    const newPost = new Post(body);
+
+    try {
+        await connect();
+
+        await newPost.save();
+
+        return new NextResponse("Post has been created", { status: 201 });
+    } catch (err) {
+        return new NextResponse("Database Error", { status: 500 });
+    }
+};
--- src/app/dashboard/page.jsx
+++ src/app/dashboard/page.jsx
@@ -4,6 +4,7 @@ import { useSession } from "next-auth/react"
 import styles from "./page.module.css"
 import useSWR from 'swr'
 import { useRouter } from 'next/navigation'
+import Image from 'next/image'
 
 const Dashboard = () => {
     // const [data, setData] = useState([])
@@ -45,9 +46,62 @@ const Dashboard = () => {
         router?.push("/dashboard/login")
     }
 
+    const handleSubmit = async (e) => {
+        e.preventDefault()
+        const title = e.target[0].value
+        const desc = e.target[1].value
+        const img = e.target[2].value
+        const content = e.target[3].value
+
+        try {
+            await fetch("/api/posts", {
+                method: "POST",
+                body: JSON.stringify({
+                    title,
+                    desc,
+                    img,
+                    content,
+                    username: session?.user.name,
+                }),
+            });
+        } catch (err) {
+            console.log(err);
+        }
+
+    }
+
     if (status === "authenticated") {
         return (
-            <div className={styles.container}>Dashboard</div>
+            <div className={styles.container}>
+                <div className={styles.posts}>
+                    {isLoading
+                        ? "loading"
+                        : data?.map((post) => (
+                            <div className={styles.post} key={post._id}>
+                                <div className={styles.imgContainer}>
+                                    <Image src={post.img} alt="" width={200} height={100} />
+                                </div>
+                                <h2 className={styles.postTitle}>{post.title}</h2>
+                                <span
+                                    className={styles.delete}
+
+                                >
+                                    X
+                                </span>
+                            </div>
+                        ))}
+                </div>
+                <form className={styles.new} onSubmit={handleSubmit}>
+                    <h1>Add New Post</h1>
+                    <input type="text" placeholder="Title" className={styles.input} />
+                    <input type="text" placeholder="Description" className={styles.input} />
+                    <input type="text" placeholder="Image" className={styles.input} />
+                    <textarea placeholder="Content" className={styles.textArea} col="30" rows="10">
+
+                    </textarea>
+                    <button className={styles.button}>Send</button>
+                </form>
+            </div>
         )
     }
 }
